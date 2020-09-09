a2a71a4a53fc2405ea4ccdf6378b00e0e3bd937f
andrewbinkard <andrewbinkard@gmail.com>
2020-09-09 15:33:54 -0500

    fix node-modules

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <div className="container">
         <InputForm />
         <PetDisplay />
-        <Config />
+        {/* <Config /> */}
       </div>
     </div>
   );

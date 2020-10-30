fb4b142b829af95717afcb70be80dabcc173e8ee
andrewbinkard <andrewbinkard@gmail.com>
2020-10-30 12:11:56 -0500

    modify db index

--- database-mongo/index.js
+++ database-mongo/index.js
@@ -1,26 +1,26 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/test");
 
 var db = mongoose.connection;
 
-db.on('error', function() {
-  console.log('mongoose connection error');
+db.on("error", function () {
+  console.log("mongoose connection error");
 });
 
-db.once('open', function() {
-  console.log('mongoose connected successfully');
+db.once("open", function () {
+  console.log("mongoose connected successfully");
 });
 
 var itemSchema = mongoose.Schema({
   quantity: Number,
-  description: String
+  description: String,
 });
 
-var Item = mongoose.model('Item', itemSchema);
+var Item = mongoose.model("Item", itemSchema);
 
-var selectAll = function(callback) {
-  Item.find({}, function(err, items) {
-    if(err) {
+var selectAll = function (callback) {
+  Item.find({}, function (err, items) {
+    if (err) {
       callback(err, null);
     } else {
       callback(null, items);
